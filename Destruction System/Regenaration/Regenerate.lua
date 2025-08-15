58fbd8408dd9cf9de80e0e3c9a515232611a4d24
l7myfr <progforlemy@gmail.com>
2025-08-15 23:01:45 +0400

    Update Regenerate.lua

--- Destruction System/Regenaration/Regenerate.lua
+++ Destruction System/Regenaration/Regenerate.lua
@@ -1,4 +1,6 @@
 local module = {}
+local ObjectPooler = require(script.Parent.Parent.ObjectPooler)
+
 local Atbs = {
 	"CastShadow",
 	"CanCollide",
@@ -20,10 +22,17 @@ function module:CopyPropeties()
 end
 function module:Regenerate()
 	self.Object.Parent = self.Object.Parent.Parent
+	for _, Part in self.VoxelHolder:GetChildren() do
+		if Part:IsA("Part") then
+			ObjectPooler.ReturnInstance(Part)
+		end
+	end
 	self.VoxelHolder:Destroy()
+
 	if self.Object:HasTag("RegenPart") then
 		self.Object:RemoveTag("RegenPart")
 	end
+	
 	self:CopyPropeties()
 end
 return module
